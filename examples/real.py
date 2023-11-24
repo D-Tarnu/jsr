920fb7814b0902d4b6369cd7cbb498ccd8899546
DTarnu <62571630+D-Tarnu@users.noreply.github.com>
2023-11-24 10:54:13 -0800

    comment

--- examples/real.py
+++ examples/real.py
@@ -9,3 +9,6 @@ mats = [
 now = datetime.now()
 invar_poly(mats, 10, 10)
 print('Time elapsed = ', (datetime.now() - now).total_seconds())
+
+# These matrices are tied to the sequence autocorrelations of Rudin-Shapiro sequences (see "On maximal autocorrelations of Rudin-Shapiro sequences" by T.).
+# Note how quickly the JSR is found compared to the complex.py example. This is due to only working with a real leading eigenvector.
